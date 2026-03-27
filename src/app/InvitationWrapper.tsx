2a6d6e2628d5c24627335b89df6e6cf6307a9267
murodjonprimkulov <primqulovmurod@gmail.com>
2026-03-27 15:48:00 +0500

    fix: correct theme mapping for Kenjabek & Snejana pink project

--- src/app/InvitationWrapper.tsx
+++ src/app/InvitationWrapper.tsx
@@ -5,7 +5,8 @@ import RolexLuxuryInvitation from '@/components/RolexLuxuryInvitation';
 import PinkLuxuryInvitation from '@/components/PinkLuxuryInvitation';
 import WatchDesignInvitation from '@/components/WatchDesignInvitation';
 import GoldClassicInvitation from '@/components/GoldClassicInvitation';
-import GoldWhiteInvitation from '@/components/PinkWhiteInvitation';
+import GoldWhiteInvitation from '@/components/GoldWhiteInvitation';
+import PinkWhiteInvitation from '@/components/PinkWhiteInvitation';
 
 interface InvitationWrapperProps {
   initialHost: string;
@@ -40,11 +41,17 @@ export default function InvitationWrapper({ initialHost }: InvitationWrapperProp
 
   if (!mounted) return null;
 
-  if (theme === 'pink') {
+  if (theme === 'pink' || initialHost.includes('pink')) {
     return (
-      <PinkLuxuryInvitation 
-        groomName="Xurshidbek"
-        brideName="Mohinur"
+      <PinkWhiteInvitation 
+        groomName="Kenjabek"
+        brideName="Snejana"
+        date="24 - APREL - 2026"
+        time="19:00"
+        locationName="Demir (Asr)"
+        locationAddress="Jizzax Shahar"
+        locationLink="https://www.google.com/maps/place/ASR+Wedding+Hall/@40.1490597,67.8229612,20.75z/data=!4m6!3m5!1s0x38b2969244164953:0xcf441bf7b030ea16!8m2!3d40.1490952!4d67.8228464!16s%2Fg%2F11h9w32rg7!5m1!1e2?entry=ttu&g_ep=EgoyMDI2MDMyMi4wIKXMDSoASAFQAw%3D%3D"
+        imageUrl="https://images.pexels.com/photos/30206324/pexels-photo-30206324/free-photo-of-elegant-gold-wedding-rings-on-marble-surface.jpeg"
         musicUrl="/assets/die_with_a_smile.mp3"
       />
     );
